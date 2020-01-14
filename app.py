5899ddd4788a5eba96c2725a699595df6a0e5c27
Brian M Walsh <bwalsh@Brian-Walshs-Computer-2.local>
2020-01-13 21:15:02 -0800

    Add app py file

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, redirect
+app = Flask(__name__)
+@app.route('/')
+def index():
+    # Return template and data
+    return render_template('index.html')
+if __name__=="__main__":
+    app.run(debug=True)
